198f8aead1b154ad6a2e41448ee38953b0caaa1b
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-07-19 03:20:47 -0300

    `reafactor`: criação do primeiro `Card`

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 export function App() {
   return (
     <>
-      <div>Hello, World!</div>
+      <div className='card'>
+        <h1>Projeto - Menu Online</h1>
+        <img src="https://utfs.io/f/0242be41-526f-4b8f-82d0-27a7d9059b40-1lzfk1.webp" width={300} alt="Imagem de site com cardápio online" />
+      </div>
     </>
   )
 }
