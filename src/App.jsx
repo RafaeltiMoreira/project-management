938d396fa0d3dceea5098c068b1f84b48c968f33
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-07-19 03:52:51 -0300

    `refactor`: criando componente `Card`

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 export function App() {
   return (
     <>
-      <div className='card'>
-        <h1>Projeto - Menu Online</h1>
-        <img src="https://utfs.io/f/0242be41-526f-4b8f-82d0-27a7d9059b40-1lzfk1.webp" width={300} alt="Imagem de site com cardápio online" />
-      </div>
+      <Card />
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card() {
+  return (
+    <div className='card'>
+      <h1>Projeto - Menu Online</h1>
+      <img src="https://utfs.io/f/0242be41-526f-4b8f-82d0-27a7d9059b40-1lzfk1.webp" width={300} alt="Imagem de site com cardápio online" />
+    </div>
+  )
+}
