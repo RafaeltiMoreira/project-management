cfce7d4b362e62e3e597eec9fcd3366fea5fd42a
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-07-21 01:17:45 -0300

    feat: adicionando biblioteca  `Ionicons` e estilização `CSS`

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,8 +4,12 @@ import './Card.css';
 export default function Card(props) {
   return (
     <div className='card'>
-      <h1>{props.item.name}</h1>
+      <h1>{props.item.title}</h1>
       <img src={props.item.image} />
+      <h2>{props.item.description}</h2>
+      <a href={props.item.deploy} target="_blank" rel="noopener noreferrer">
+        <ion-icon name="logo-github"></ion-icon>
+      </a>
     </div>
   )
 }
